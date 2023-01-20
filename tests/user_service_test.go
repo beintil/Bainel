6070b5dfd7920c2c6b8bb2a1193759b863a68293
beintil <sergeisavilov4@gmail.com>
2023-01-20 15:31:31 +0700

    Добавил тест к регистрации

--- tests/user_service_test.go
+++ tests/user_service_test.go
@@ -1,6 +1,7 @@
-package user_controllers
+package tests
 
 import (
+	"RegisterUser/internal/app/controller/user_controllers"
 	"RegisterUser/repository/user"
 	"bytes"
 	"encoding/json"
@@ -11,15 +12,16 @@ import (
 
 func TestRegisterUser(t *testing.T) {
 	// Создаем тестовый сервер
-	ts := httptest.NewServer(http.HandlerFunc(RegisterUser))
+	ts := httptest.NewServer(http.HandlerFunc(user_controllers.RegisterUser))
 	defer ts.Close()
 
-	// Создаем тестовый запрос
-	user := user.User{NickName: "test", Email: "teswrwr2", Password: "testpassword"}
-	jsonUser, err := json.Marshal(user)
+	// Создаем тестовый запрос, но тк email не должен повторятся, то меняйте название email для каждого теста
+	testUser := user.User{NickName: "test", Email: "testmail422", Password: "testpassword"}
+	jsonUser, err := json.Marshal(testUser)
 	if err != nil {
 		t.Fatal(err)
 	}
+	// Кодируем наше тело user в виде json и используем метод POST, проверяя ошибку
 	req, err := http.NewRequest("POST", ts.URL, bytes.NewBuffer(jsonUser))
 	if err != nil {
 		t.Fatal(err)

9a4884e8114421eb47a574a84ed222322694a767
beintil <sergeisavilov4@gmail.com>
2023-01-22 12:12:59 +0700

    Возможность Авторизовываться и заходить на домашнюю страницу

--- internal/app/handler/handlers.go
+++ internal/app/handler/handlers.go
@@ -2,10 +2,15 @@ package handler
 
 import (
 	"RegisterUser/internal/app/controller/user_controllers"
+	"RegisterUser/internal/app/home"
 	"github.com/gorilla/mux"
 )
 
 func Handler(router *mux.Router) {
 	router.HandleFunc("/users/{id}", user_controllers.GetUsers).Methods("GET")
 	router.HandleFunc("/register/", user_controllers.RegisterUser).Methods("POST")
+	router.HandleFunc("/login/", user_controllers.Login).Methods("GET")
+	{
+		router.HandleFunc("/home/", home.Home).Methods("GET")
+	}
 }

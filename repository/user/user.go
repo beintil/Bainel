1fc4b2e4891c9e472af532a9e7db9ae50b006e44
beintil <sergeisavilov4@gmail.com>
2023-01-22 12:20:28 +0700

    Добавлена структура для авторизации

--- repository/user/user.go
+++ repository/user/user.go
@@ -11,7 +11,6 @@ type User struct {
 	Password string             `bson:"password" json:"password"`
 }
 
-// Authentication
 type Login struct {
 	Email    string `bson:"email" json:"email"`
 	Password string `bson:"password" json:"password"`
